1816725fed0600b4d7670869da473964df26248b
GeorgeSearz <60758943+GeorgeSearz@users.noreply.github.com>
2020-03-08 00:30:24 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const http = require("http");
 const socketIo = require("socket.io");
 const axios = require("axios");
 
-const port = process.env.PORT || 4001;
+const port = 80;
 const index = require("./routes/index");
 
 const app = express();
